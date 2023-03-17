88a5f35fef93a33088a0fe6250caa0b02d4aac43
Vitalii Boiko <107396176+vitaliiseeker@users.noreply.github.com>
2023-03-17 17:47:29 +0200

    Update Layout.styled.js

--- src/components/Layout/Layout.styled.js
+++ src/components/Layout/Layout.styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
-import { ReactComponent as Github } from "../../images/github.svg";
+import { ReactComponent as Github } from "images/github.svg";
 
 
 export const Nav = styled.nav`
